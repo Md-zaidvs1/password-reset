86514b1efe866456260ba73637c2539b87ed4f14
Md-zaidvs1 <mdzaidv573@gmail.com>
2026-04-03 07:55:53 +0530

    fix: add default redirect to forgot-password

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css"; 
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import ForgotPassword from "./components/ForgotPassword";
 import ResetPassword from "./components/ResetPassword";
 
@@ -9,9 +8,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/forgot-password" />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        
-        {/* We use :token here so React knows there's a dynamic link */}
         <Route path="/reset-password/:token" element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
